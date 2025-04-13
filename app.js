3e645e68b94e5fb568226d23054e7fc3e60d2895
Raiyan Nawaz <91454967+raiyannawaz@users.noreply.github.com>
2025-04-13 17:12:06 +0530

    API error fixed app.js

--- app.js
+++ app.js
@@ -33,16 +33,22 @@ app.use(express.static(path.join(__dirname, '/public')))
 // HOME 
 
 app.get('/', auth, async (req, res) => {
+    try{
+        let _id = req._id;
+        let user = await User.findOne({ _id })
     
-    let _id = req._id;
-
-    let user = await User.findOne({ _id })
-    console.log(user)
-
-    res.status(200)
-    res.render('index', {
-        title: 'Textify'
-    })
+        res.status(200)
+        res.render('index', {
+            title: 'Textify',
+            name: user.name.split(' ')[1] || user.name.split(' ')[0]
+        })
+    }
+    catch(err){
+        res.status(404)
+        res.render('index', {
+            title: 'Textify'
+        })
+    }
 })
 
 // HOME 
@@ -344,7 +350,6 @@ app.get('/translate', auth, (req, res) => {
     })
 })
 
-
 app.post('/translate', async (req, res) => {
 
     const body = {
@@ -376,33 +381,20 @@ app.post('/translate', async (req, res) => {
         }
         else {
 
-            const options = {
-                method: 'POST',
-                url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
-                params: {
-                    'to[0]': body.to,
-                    'api-version': '3.0',
-                    profanityAction: 'NoAction',
-                    textType: 'plain'
-                },
-                headers: {
-                    'content-type': 'application/json',
-                    'X-RapidAPI-Key': '6d3682af21mshb332e6fed9d3529p1f9390jsn5715d3cf30a7',
-                    'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-                },
-                data: [
-                    {
-                        Text: body.texts
-                    }
-                ]
-            };
+            const url = `https://translation.googleapis.com/language/translate/v2?key=AIzaSyAbftD9JftW3VqdfzCsUK3sqkfvXGPqXJc`
 
-            const response = await axios.request(options);
+            const response = await axios.post(url, {
+                q: body.texts,
+                target: body.to
+            });
+
+            let data = await response.data.data
 
             res.status(200)
             res.render('translate', {
                 title: 'Textify',
-                texts: response.data[0].translations[0].text
+                to: body.to,
+                texts: data.translations[0].translatedText
             })
         }
     } catch (error) {
@@ -433,7 +425,6 @@ app.post('/dictionary', async (req, res) => {
     let texts = req.body.texts;
 
     try {
-
         if (texts === '') {
             res.status(300)
             res.render('dictionary', {
@@ -444,103 +435,105 @@ app.post('/dictionary', async (req, res) => {
         else {
             let url = `https://api.dictionaryapi.dev/api/v2/entries/en/` + texts;
 
-            axios.request(url).then((response) => {
+            let response = (await axios.get(url))
 
-                let data = response.data;
+            let data = await response.data
 
-                const getMeanings = () => {
-                    let totalMeanings = []
-                    for (let i = 0; i < data.length; i++) {
-                        let meanings = data[i].meanings;
-                        for (let j = 0; j < meanings.length; j++) {
-                            let definitions = meanings[j].definitions;
-                            for (let k = 0; k < definitions.length; k++) {
-                                let definition = definitions[k].definition
-                                definition !== undefined ? totalMeanings.push(definition) : ''
+            const getMeanings = () => {
+                let totalMeanings = []
+                for (let i = 0; i < data.length; i++) {
+                    let meanings = data[i].meanings;
+                    for (let j = 0; j < meanings.length; j++) {
+                        let definitions = meanings[j].definitions;
+                        for (let k = 0; k < definitions.length; k++) {
+                            let definition = definitions[k].definition
+                            definition !== undefined ? totalMeanings.push(definition) : ''
+                        }
+                    }
+                }
+                let meanings = totalMeanings.sort((a, b) => {
+                    if (a.length > b.length)
+                        return -1;
+                    if (a.length < b.length)
+                        return 1;
+                    return 0;
+                })[0]
+
+                return meanings
+            }
+
+            const getExamples = () => {
+                let totalExamples = []
+                for (let i = 0; i < data.length; i++) {
+                    let meanings = data[i].meanings;
+                    for (let j = 0; j < meanings.length; j++) {
+                        let definitions = meanings[j].definitions;
+                        for (let k = 0; k < definitions.length; k++) {
+                            let example = definitions[k].example
+                            example === undefined ? '' : totalExamples.push(example)
+                        }
+                    }
+                }
+                let examples = totalExamples.sort((a, b) => {
+                    if (a.length > b.length)
+                        return -1;
+                    if (a.length < b.length)
+                        return 1;
+                    return 0;
+                })[0]
+
+                return examples
+            }
+
+            const getSynonyms = () => {
+                let totalSynonyms = []
+                for (let i = 0; i < data.length; i++) {
+                    let meanings = data[i].meanings;
+                    for (let j = 0; j < meanings.length; j++) {
+                        totalSynonyms = totalSynonyms.concat(meanings[j].synonyms)
+                        let definitions = meanings[j].definitions;
+                        for (let k = 0; k < definitions.length; k++) {
+                            let synonyms = definitions[k].synonyms
+                            for (let l = 0; l < synonyms.length; l++) {
+                                let synonym = synonyms[l]
+                                totalSynonyms.push(synonym)
                             }
                         }
                     }
-                    let meanings = totalMeanings.sort((a, b) => {
-                        if (a.length > b.length)
-                            return -1;
-                        if (a.length < b.length)
-                            return 1;
-                        return 0;
-                    })[0]
-
-                    return meanings
                 }
+                let synonyms = totalSynonyms.slice(0, 5).join(', ')
+                return synonyms
+            }
 
-                const getExamples = () => {
-                    let totalExamples = []
-                    for (let i = 0; i < data.length; i++) {
-                        let meanings = data[i].meanings;
-                        for (let j = 0; j < meanings.length; j++) {
-                            let definitions = meanings[j].definitions;
-                            for (let k = 0; k < definitions.length; k++) {
-                                let example = definitions[k].example
-                                example === undefined ? '' : totalExamples.push(example)
+            const getAntonyms = () => {
+                let totalAntonyms = []
+                for (let i = 0; i < data.length; i++) {
+                    let meanings = data[i].meanings;
+                    for (let j = 0; j < meanings.length; j++) {
+                        totalAntonyms = totalAntonyms.concat(meanings[j].antonyms)
+                        let definitions = meanings[j].definitions;
+                        for (let k = 0; k < definitions.length; k++) {
+                            let antonyms = definitions[k].antonyms
+                            for (let l = 0; l < antonyms.length; l++) {
+                                let antonym = antonyms[l]
+                                totalAntonyms.push(antonym)
                             }
                         }
                     }
-                    let examples = totalExamples.sort((a, b) => {
-                        if (a.length > b.length)
-                            return -1;
-                        if (a.length < b.length)
-                            return 1;
-                        return 0;
-                    })[0]
-
-                    return examples
                 }
+                let antonyms = totalAntonyms.slice(0, 5).join(', ')
+                return antonyms
+            }
 
-                const getSynonyms = () => {
-                    let totalSynonyms = []
-                    for (let i = 0; i < data.length; i++) {
-                        let meanings = data[i].meanings;
-                        for (let j = 0; j < meanings.length; j++) {
-                            let definitions = meanings[j].definitions;
-                            for (let k = 0; k < definitions.length; k++) {
-                                let synonyms = definitions[k].synonyms
-                                for (let l = 0; l < synonyms.length; l++) {
-                                    let synonym = synonyms[l]
-                                    totalSynonyms.push(synonym)
-                                }
-                            }
-                        }
-                    }
-                    let synonyms = totalSynonyms.slice(0, 5).join(', ')
-                    return synonyms
-                }
+            let meaning = getMeanings();
+            let example = getExamples();
+            let synonyms = getSynonyms();
+            let antonyms = getAntonyms();
 
-                const getAntonyms = () => {
-                    let totalAntonyms = []
-                    for (let i = 0; i < data.length; i++) {
-                        let meanings = data[i].meanings;
-                        for (let j = 0; j < meanings.length; j++) {
-                            let definitions = meanings[j].definitions;
-                            for (let k = 0; k < definitions.length; k++) {
-                                let antonyms = definitions[k].antonyms
-                                for (let l = 0; l < antonyms.length; l++) {
-                                    let antonym = antonyms[l]
-                                    totalAntonyms.push(antonym)
-                                }
-                            }
-                        }
-                    }
-                    let antonyms = totalAntonyms.slice(0, 5).join(', ')
-                    return antonyms
-                }
-
-                let meaning = getMeanings();
-                let example = getExamples();
-                let synonyms = getSynonyms();
-                let antonyms = getAntonyms();
-
-                res.status(200)
-                res.render('dictionary', {
-                    title: 'Textify',
-                    texts: `${meaning ? 'Meaning: ' + meaning : ''}
+            res.status(200)
+            res.render('dictionary', {
+                title: 'Textify',
+                texts: `${meaning ? 'Meaning: ' + meaning : ''}
 
 ${example ? 'Example: ' + example : ''}
 
@@ -548,15 +541,6 @@ ${synonyms ? 'Synonyms: ' + synonyms : ''}
 
 ${antonyms ? 'Antonyms: ' + antonyms : ''}`
 
-                })
-
-            }).catch((err) => {
-                res.status(400)
-                res.render('dictionary', {
-                    title: 'Textify',
-                    texts: 'Failed!'
-                })
-                console.log(err)
             })
         }
     }
@@ -688,43 +672,37 @@ app.post('/grammercheck', async (req, res) => {
         }
         else {
 
+            let url = 'https://api.languagetool.org/v2/check'
+
             const options = {
-                method: 'POST',
-                url: 'https://api.edenai.run/v2/text/spell_check',
-                headers: {
-                    accept: 'application/json',
-                    'content-type': 'application/json',
-                    authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDk2OWExODMtZDg5Ny00NGU4LWIzMzUtNTlkNmNlZjk1MjhiIiwidHlwZSI6ImFwaV90b2tlbiJ9.A8s59aBopzeUeSxNe7F4dIlBPDZEFYlSGzkOPzzXKW0'
+                params: {
+                    text: texts,
+                    language: 'en-US'
                 },
-                data: {
-                    response_as_dict: true,
-                    attributes_as_list: false,
-                    show_original_response: false,
-                    providers: 'nlpcloud,openai,microsoft,cohere,sapling,prowritingaid',
-                    language: 'en',
-                    text: texts
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
                 }
-            };
+            }
 
-            axios.request(options).then((response) => {
+            let response = await axios.post(url, null, options)
+            let data = await response.data
 
-                response.data.nlpcloud.items.forEach((data) => {
+            let newTexts = texts
+            let offsetShift = 0
 
-                    res.status(200)
-                    res.render('grammercheck', {
-                        title: 'Textify',
-                        texts: data.suggestions[0].suggestion
-                    })
+            for (let match of data.matches) {
+                const replacement = match.replacements[0].value
+                const start = match.offset + offsetShift
+                const end = start + match.length
+                newTexts = newTexts.slice(0, start) + replacement + newTexts.slice(end)
+                offsetShift += replacement.length - match.length
+            }
+
+            res.status(200)
+            res.render('grammercheck', {
+                title: 'Textify',
+                texts: newTexts
                 })
-
-            }).catch((err) => {
-                console.log(err)
-                res.status(400)
-                res.render('grammercheck', {
-                    title: 'Textify',
-                    texts: 'Failed!'
-                })
-            })
 
         }
     }
